89a7862c26804ac8459a70a29d5f02b3ece82769
Richman | RO'Lyfe <richman@rootoflyfe.com>
2026-07-18 07:53:13 -0400

    Add header comment to irr-calculator.js
    Added header comment to describe the IRR calculator.

--- calculators/irr-calculator.js
+++ calculators/irr-calculator.js
@@ -1,1 +1,8 @@
-
+/*
+=====================================
+RO'Lyfe IRR Calculator
+Version: 1.0
+Purpose:
+Calculates internal rate of return for investment scenarios.
+=====================================
+*/
